ea8cd09ac7edadc9696d59c050d2796e692e63e6
mertlkn <m558mert@gmail.com>
2023-11-15 01:20:38 +0100

    custom scroll bar uploaded

---
+++ CustomHandle.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using deVoid.UIFramework;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+public class CustomHandle : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    public event Action<float> OnValueChanged;
+    public event Action OnPointerDownAction;
+    public event Action OnPointerUpAction;
+    public event Action OnValueSet;
+
+    [SerializeField] private RectTransform visualRect;
+    private float _originalRectHeight;
+    private bool _isOriginalRectHeightSet = true;
+
+    private float _shrinkMultiplier = 200f;
+
+    private float _tempHeightVal;
+    private Vector3 _tempLocalPos;
+
+    private bool _isDragging;
+    private float _minY;
+    private float _maxY;
+    private Transform _transform;
+    private float _touchOffset;
+
+    private void Awake()
+    {
+        _originalRectHeight = visualRect.sizeDelta.y;
+        _transform = transform;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _isDragging = true;
+        _touchOffset = _transform.position.y - UIFrameHelper.GetUICameraToWorldPosition(Input.mousePosition).y;
+        OnPointerDownAction?.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _isDragging = false;
+        _touchOffset = 0f;
+        OnPointerUpAction?.Invoke();
+    }
+
+    public void SetMinMaxY(float minY, float maxY)
+    {
+        _minY = minY;
+        _maxY = maxY;
+        var position = transform.position;
+        position.y = _maxY;
+        transform.position = position;
+    }
+
+    private void Update()
+    {
+        if (_isDragging)
+        {
+            var position = transform.position;
+            position.y = Mathf.Clamp(UIFrameHelper.GetUICameraToWorldPosition(Input.mousePosition).y + _touchOffset
+                , _minY, _maxY);
+            transform.position = position;
+            OnValueChanged?.Invoke((position.y - _minY) / (_maxY - _minY));
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void SetValue(float val)
+    {
+        if (_isDragging) return;
+
+        OnValueSet?.Invoke();
+
+        var position = _transform.position;
+        position.y = Mathf.Lerp(_minY, _maxY, val);
+
+        if (val > 1f)
+        {
+            // make handle smaller,  so it would look like it is shrinking
+            _tempHeightVal = _originalRectHeight * (1f - (val - 1f) * _shrinkMultiplier);
+            visualRect.sizeDelta = new Vector2(visualRect.sizeDelta.x, _tempHeightVal);
+            _isOriginalRectHeightSet = false;
+            _transform.position = position;
+            _tempLocalPos = _transform.localPosition;
+            _tempLocalPos.y += (_originalRectHeight - _tempHeightVal) * 0.5f;
+            _transform.localPosition = _tempLocalPos;
+
+        }
+        else if (val < 0f)
+        {
+            // make handle smaller,  so it would look like it is shrinking
+            _tempHeightVal = _originalRectHeight * (1f + val * _shrinkMultiplier);
+            visualRect.sizeDelta = new Vector2(visualRect.sizeDelta.x, _tempHeightVal);
+            _isOriginalRectHeightSet = false;
+            _transform.position = position;
+            _tempLocalPos = _transform.localPosition;
+            _tempLocalPos.y -= (_originalRectHeight - _tempHeightVal) * 0.5f;
+            _transform.localPosition = _tempLocalPos;
+        }
+        else if (!_isOriginalRectHeightSet)
+        {
+            visualRect.sizeDelta = new Vector2(visualRect.sizeDelta.x, _originalRectHeight);
+            _isOriginalRectHeightSet = true;
+            _transform.position = position;
+        }
+        else
+        {
+            _transform.position = position;
+        }
+    }
+
+    public void SetSize(float size)
+    {
+        visualRect.sizeDelta = new Vector2(visualRect.sizeDelta.x, size);
+        _originalRectHeight = visualRect.sizeDelta.y;
+    }
+
+    public void SetShrinkMultiplier(float shrinkMultiplier)
+    {
+        _shrinkMultiplier = shrinkMultiplier;
+    }
+}
---
+++ CustomScrollBar.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using DG.Tweening;
+using Game.Enum;
+using UI.Common;
+using UnityEngine;
+
+
+public class CustomScrollBar : MonoBehaviour
+{
+    [SerializeField] private CustomHandle handle;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private MirroredSlicedImage image;
+    [SerializeField] private Sprite defaultSprite;
+    [SerializeField] private Sprite holdSprite;
+
+    [SerializeField] private RectTransform top;
+    [SerializeField] private RectTransform bottom;
+
+    public event Action<float> OnValueChanged;
+
+    private float _minSize = 200f;
+    private bool _shouldHide;
+    private float _lastVal;
+
+    private bool _fadeTweenIsPlaying;
+    private Tween _fadeTween;
+    private const float _shrinkMultiplierConstant = 800f;
+
+    private void Start()
+    {
+        handle.OnValueChanged += OnValueChanged;
+        handle.OnPointerDownAction += OnPointerDown;
+        handle.OnPointerUpAction += OnPointerUp;
+        FadeOutScrollBar();
+    }
+
+    public void Initialize(float viewportHeight, float contentHeight)
+    {
+        var size = viewportHeight / contentHeight * viewportHeight;
+        size = Mathf.Clamp(size, _minSize, viewportHeight);
+
+        _shouldHide = size >= viewportHeight;
+
+        if (_shouldHide)
+        {
+            HideScrollBar();
+        }
+
+        handle.SetSize(size);
+
+        top.anchoredPosition = new Vector2(0, -size / 2);
+        bottom.anchoredPosition = new Vector2(0, size / 2);
+
+        handle.SetMinMaxY(bottom.position.y, top.position.y);
+
+        handle.SetShrinkMultiplier(contentHeight / _shrinkMultiplierConstant);
+    }
+
+    public void SetValue(float val)
+    {
+        handle.SetValue(val);
+
+        if (Math.Abs(_lastVal - val) < 2e-05)
+        {
+            FadeOutScrollBar();
+        }
+        else
+        {
+            ShowScrollBar();
+        }
+        _lastVal = val;
+    }
+
+    private void OnPointerDown()
+    {
+        HoldScrollBar();
+        ShowScrollBar();
+        HapticManager.FakeButtonClickRequested(ButtonClickType.ButtonClick);
+    }
+
+    private void OnPointerUp()
+    {
+        ReleaseScrollBar();
+        FadeOutScrollBar();
+        HapticManager.FakeButtonClickRequested(ButtonClickType.ButtonUnClick);
+    }
+
+    private void HoldScrollBar()
+    {
+        var sizeDelta = image.rectTransform.sizeDelta;
+        sizeDelta = new Vector2(holdSprite.bounds.size.x * holdSprite.pixelsPerUnit, sizeDelta.y);
+        image.rectTransform.sizeDelta = sizeDelta;
+        image.sprite = holdSprite;
+    }
+
+    private void ReleaseScrollBar()
+    {
+        var sizeDelta = image.rectTransform.sizeDelta;
+        sizeDelta = new Vector2(defaultSprite.bounds.size.x * holdSprite.pixelsPerUnit, sizeDelta.y);
+        image.rectTransform.sizeDelta = sizeDelta;
+        image.sprite = defaultSprite;
+    }
+
+
+    public void ShowScrollBar()
+    {
+        if (_fadeTweenIsPlaying)
+        {
+            _fadeTween.Kill();
+            _fadeTweenIsPlaying = false;
+        }
+
+        if (_shouldHide) return;
+
+        canvasGroup.alpha = 1;
+    }
+
+    private void HideScrollBar()
+    {
+        if (_fadeTweenIsPlaying)
+        {
+            _fadeTween.Kill();
+            _fadeTweenIsPlaying = false;
+        }
+        canvasGroup.alpha = 0;
+    }
+
+    private void FadeOutScrollBar()
+    {
+        if (_fadeTweenIsPlaying)
+        {
+            return;
+        }
+        _fadeTween = canvasGroup.DOFade(0, 2).SetEase(Ease.InExpo);
+        _fadeTweenIsPlaying = true;
+    }
+}
